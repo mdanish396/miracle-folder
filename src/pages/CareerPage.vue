c0314192fe36efbc255ebc62c44a38825fb527ca
mdanish396 <danishazhar186@gmail.com>
2025-01-16 17:57:58 +0800

    career page saja man

--- src/pages/CareerPage.vue
+++ src/pages/CareerPage.vue
@@ -1,12 +1,24 @@
 <template>
   <q-page>
-    <!-- Hero Image (Full Width) -->
-    <div class="hero-section">
-      <div class="hero-image">
-        <img
-        src="src/assets/career1.png"
-        alt="Hero Image"/>
-        <div class="gradient-overlay"></div>
+    <!-- Hero Image -->
+    <div class="new-hero">
+      <div class="new-container">
+        <!-- Text Content Section -->
+        <div class="text-content">
+          <div class="line-hero-holder">
+            <div class="line-hero">
+              <div class="line-hero-1"></div>
+            </div>
+          </div>
+          <h2 class="text-above">Career</h2>
+          <p class="text-below">
+            Explore our current job opportunities.          </p>
+        </div>
+
+        <!-- Image Section -->
+        <div class="career-image-container">
+          <img src="src/assets/currentproject/house1.jpg" class="top-image" />
+        </div>
       </div>
     </div>
 
@@ -16,10 +28,10 @@
         <div class="text-content-1">
           <h2>Why Join Us</h2>
           <ul>
-            <li><strong>Flat structure:</strong> Hierarchy is not our focus; we value contributions from all levels.</li>
-            <li><strong>Fast career progression:</strong> We ensure you have the tools and opportunities to grow.</li>
-            <li><strong>Vibrant Work Culture:</strong> We celebrate collaboration and diversity.</li>
-            <li><strong>Realistic bosses:</strong> Our leaders are supportive and result-oriented.</li>
+            <li><div class="list"> <div class="strong-list"><strong>Realistic bosses</strong></div><div class="dot">:</div> <div>  Our leaders are supportive and result-oriented.</div></div></li>
+            <li><div class="list"> <div class="strong-list"><strong>Flat structure</strong></div> <div class="dot">:</div><div> Hierarchy is not our focus; we value contributions from all levels.</div></div></li>
+            <li><div class="list"> <div class="strong-list"><strong>Fast career progression</strong> </div><div class="dot">:</div><div>  We ensure you have the tools and opportunities to grow.</div></div></li>
+            <li><div class="list"> <div class="strong-list"><strong>Vibrant Work Culture</strong></div><div class="dot">:</div> <div>  We celebrate collaboration and diversity.</div></div></li>
           </ul>
         </div>
         <div class="image-container">
@@ -63,7 +75,9 @@
           <img src="src/assets/career2.png" alt="Why Join Us" class="career-image" />
         </div>
       </div>
-      <q-btn flat label="Career Opportunities" class="btn-story" to="/about-miracle"/>
+    </div>
+    <div class="btn">
+    <q-btn flat label="Career Opportunities" class="btn-story" to='/career-opportunities'/>
     </div>
   </q-page>
 </template>
@@ -73,31 +87,212 @@
 </script>
 
 <style scoped>
-.hero-section {
-  position: relative;
-  height: 47vh; /* Full-screen height */
-  overflow: hidden;
-  margin-top: -70px;
+
+@font-face {
+  font-family: 'TitilliumWebRegular';
+  src: url('src/assets/fonts/TitilliumWeb-Regular.ttf') format('truetype');
+  font-weight: bold;
 }
 
-.hero-image img {
+@font-face {
+  font-family: 'TitilliumWebSemiBold';
+  src: url('src/assets/fonts/TitilliumWeb-SemiBold.ttf') format('truetype');
+  font-weight: bold;
+}
+
+@font-face {
+  font-family: 'RecklessNeueMedium';
+  src: url('src/assets/fonts/RecklessNeue-Medium.ttf') format('truetype');
+}
+
+@font-face {
+  font-family: 'TitilliumWebBold';
+  src: url('src/assets/fonts/TitilliumWeb-Bold.ttf') format('truetype');
+}
+
+@font-face {
+  font-family: 'AvenirMedium';
+  src: url('src/assets/fonts/Avenir LT Std 65 Medium.otf') format('opentype');
+}
+
+.new-hero {
+  background-color: #0d182b;
+  display: flex;
+  height: 71vh;
+  justify-content: center;
+  align-items: center;
+}
+
+.new-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   width: 100%;
-  height: 70vh;
+}
+
+.text-content {
+  flex: 1;
+  padding-left: 6%;
+}
+
+.line-hero-holder {
+  position: static;
+  margin-top: -20px;
+  padding-bottom: 30px;
+}
+
+.line-hero {
+  display: flex;
+  width: 12px; /* Thickness of the line */
+  height: 4px; /* Height of the line */
+  background-color: #d9d323; /* Gold accent */
+  padding-inline: 40px;
+
+}
+
+.line-hero-1 {
+  display: inline-block;
+  height: 2px; /* Height of the line */
+  background-color: #a7a4a4; /* Gold accent */
+  margin-top: 1px; /* Space between the line and text */
+  padding-inline-end: 140px;
+  margin-inline-start: 40px;
+}
+
+.text-above {
+  font-family: 'RecklessNeueMedium';
+  font-size: 48px;
+  margin: 0;
+}
+
+.text-below {
+  font-family: 'TitilliumWebRegular';
+  font-size: 16px;
+  padding-top: 20px;
+  padding-right: 40px;
+  color: #fff;
+}
+
+.text-above {
+  color: #fff;
+}
+
+/* Image Section */
+.career-image-container {
+  flex: 1; /* Adjusts the width for the image section to make it more than half the screen width */
+  display:flex;
+  justify-content: flex-end;
+  align-items:flex-end;
+}
+
+.top-image {
+  width: 850px;
+  height: 71vh;
   object-fit: cover;
 }
 
-.gradient-overlay {
-  position: absolute;
-  top: 0;
-  left: 0;
+@media (max-width: 1260px) {
+
+.text-above {
+  font-size: 30px;
+}
+
+.text-below {
+  font-size: 15px;
+}
+
+.line-hero {
+padding-inline: 20px;
+}
+
+.line-hero-1 {
+  padding-inline-end: 100px;
+  margin-inline-start: 20px;
+}
+
+.top-image {
+  width: 700px;
+}
+}
+
+@media (max-width: 1024px) {
+  .top-image {
+  width: 600px;
+}
+}
+
+@media (max-width: 910px) {
+  .top-image {
+  width: 500px;
+}
+}
+
+@media (max-width: 820px) {
+
+  .new-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+  }
+
+  .career-image-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: -460px;
+  }
+
+  .text-content {
+    padding-left: 0px;
+    padding-top: 175px;
+  }
+
+  .text-above {
+  font-size: 30px;
+}
+
+.text-below {
+  padding-right: 0px;
+
+}
+
+.line-hero-holder {
+  display: none;
+}
+
+.top-image {
   width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.4);
-  z-index: 2;
+  max-width: 500px;
+  height: 47vh;
+  }
+}
+@media (max-width: 520px) {
+
+  .career-image-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: -450px;
+  }
+
+  .text-content {
+    padding-left: 0px;
+    padding-top: 168px;
+  }
+
+  .text-above {
+    margin-bottom: -20px;
+  }
+
+  .top-image {
+  width: 100%;
+  height: 49vh;
+  }
 }
 
 .career-section {
-  padding: 80px 40px;
+  padding: 80px 10%;
 }
 
 .content-container {
@@ -119,21 +314,40 @@
 }
 
 .text-content-1 h2 {
-  font-family: 'Georgia', serif;
-  font-size: 2.5rem;
+  font-family: 'RecklessNeueMedium';
+  font-size: 42px;
   color: #000000;
   margin-bottom: 20px;
 }
 
 .text-content-2 h2 {
-  font-family: 'Georgia', serif;
-  font-size: 2.5rem;
+  font-family: 'RecklessNeueMedium';
+  font-size: 42px;
   color: #000000;
   margin-bottom: 20px;
 }
 
+.list {
+  display: flex;
+  width: 450px;
+  /* border: 2px solid black; */
+}
+
+.strong-list {
+width: 140px;
+font-family: 'TitilliumWebSemiBold';
+/* border: 2px solid black; */
+flex-shrink: 0;
+}
+
+.dot {
+  padding-right: 10px;
+  font-family: 'TitilliumWebSemiBold';
+}
+
 .text-content-1 li {
   font-size: 18px;
+  font-family: 'TitilliumWebRegular';
   color: #000000;
   line-height: 1.2;
   margin-bottom: 20px;
@@ -142,6 +356,7 @@
 .text-content-1 p {
   font-size: 18px;
   color: #000000;
+  font-family: 'TitilliumWebRegular';
   line-height: 1.6;
   margin-bottom: 20px;
 }
@@ -149,6 +364,7 @@
 .text-content-2 p {
   font-size: 18px;
   color: #000000;
+  font-family: 'TitilliumWebRegular';
   line-height: 1.6;
   margin-bottom: 20px;
 }
@@ -163,7 +379,7 @@
 
 /* Image */
 .career-image {
-  max-width: 100%;
+  width: 70vw;
   height: auto;
   border-radius: 2px;
 }
@@ -173,38 +389,39 @@ ul {
   list-style: disc;
 }
 
+.btn {
+  text-align: center;
+}
+
 .btn-story {
   background-color: transparent;
-  border: 2px solid #02947e;
+  border: 2px solid black;
   color: #000000;
-  font-weight: bold;
-  padding: 10px 40px;
-  margin-top: 100px;
-  left: 40%;
+  font-family: 'AvenirMedium';
+  font-size: 17px;
+  padding: 10px 20px;
+  text-align: center;
+  width: 300px;
+  height: 60px;
+  margin-top: 10px;
+  margin-bottom: 20px;
   border-radius: 0;
   transition: background-color 0.3s ease, color 0.3s ease, transform 0.3s ease;
 }
 
 .btn-story:hover {
-  background-color: #00B398;
-  color: white;
+  background-color: #a39f1a;
+  border: 2px solid #a39f1a;
+  color: #fff;
   transform: translateY(-3px);
-}
-
-@media (min-width: 1110px) {
-  .hero-section {
-    margin-top: -70px;
-}
-}
-@media (max-width: 1106px) {
-  .hero-section {
-    height: 48vh;
-    margin-top: -85px;
-}
 }
 
 /* Adjust layout for smaller screens */
 @media (max-width: 1024px) {
+
+  .career-section {
+    padding-top: 10px;
+  }
   .content-container {
     flex-direction: column;
     align-items: center;
@@ -214,6 +431,10 @@ ul {
   .text-content-2 {
     padding-right: 0px;
     padding-bottom: 0px;
+  }
+
+  .text-content-2 {
+    order: -1;
   }
 
   .text-content-1 {
@@ -240,10 +461,6 @@ ul {
     margin-bottom: 20px;
   }
 
-  .text-content-2 {
-    order: -1;
-  }
-
   .image-container {
     width: 100%;
   }
@@ -257,9 +474,6 @@ ul {
 
 @media (max-width: 768px) {
   /* Tablet Adjustments */
-  .career-section {
-    padding: 12px;
-  }
 
   .text-content-1 h2 {
     font-size: 1.9rem;
@@ -285,19 +499,10 @@ ul {
   .text-content-2 p {
     font-size: 17px;
   }
-
-  .btn-story {
-    left: 24%;
-    margin-top: 40px;
-    margin-bottom: 30px;
-  }
 }
 
 @media (max-width: 480px) {
   /* Phone Adjustments */
-  .career-section {
-    padding: 8px;
-  }
 
   h2 {
     font-size: 1.25rem;
